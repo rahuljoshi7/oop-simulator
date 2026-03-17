36076244b2a8bb0d58d4667d25a662a90ad6c50d
Rahul <rahulmjoshi0007@gmail.com>
2026-03-17 22:54:21 +0530

    added gunicorn

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://oop_db_7dol_user:bVjSnbt6rLaSqA1VO98M7vKjrsjyyIA3@dpg-d6sfg375gffc738in6sg-a/oop_db_7dol")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "your_secret_key"
+
+    # //postgresql://oop_db_7dol_user:bVjSnbt6rLaSqA1VO98M7vKjrsjyyIA3@dpg-d6sfg375gffc738in6sg-a/oop_db_7dol
