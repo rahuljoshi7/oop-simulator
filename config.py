80d790263cac2f0a942aca9d91ff148a019e8a78
Rahul <rahulmjoshi0007@gmail.com>
2026-03-17 23:10:13 +0530

    final fix for db

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://oop_db_7dol_user:bVjSnbt6rLaSqA1VO98M7vKjrsjyyIA3@dpg-d6sfg375gffc738in6sg-a/oop_db_7dol")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "your_secret_key"
-
-    # //postgresql://oop_db_7dol_user:bVjSnbt6rLaSqA1VO98M7vKjrsjyyIA3@dpg-d6sfg375gffc738in6sg-a/oop_db_7dol
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret")
